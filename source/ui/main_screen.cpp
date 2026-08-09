09a62348a1f88f90a4f55ed83a2a758f2a3ba549
Mr. Myxa <thisisandrew0110@gmail.com>
2026-08-09 13:45:15 +0200

    rewr: move screen to members

--- source/ui/main_screen.cpp
+++ source/ui/main_screen.cpp
@@ -2,69 +2,76 @@
 
 #include <chrono>
 #include <ftxui/component/component.hpp>
+#include <ftxui/screen/screen.hpp>
 
 #include "custom_events.hpp"
 #include "monitor_tabs_view.hpp"
 
 namespace ui
 {
+    MainScreen::MainScreen() : screen_(ftxui::ScreenInteractive::Fullscreen()) {}
+
     int MainScreen::Run()
     {
         controller_.Refresh();
         MonitorTabsView tabs_view(controller_);
 
-        auto screen = ftxui::ScreenInteractive::Fullscreen();
-        StartRefreshThread(screen);
+        StartRefreshThread();
 
         // clang-format off
         auto component = ftxui::CatchEvent(tabs_view.Component(), [&](ftxui::Event event) -> bool {
             if (event == ftxui::Event::Character('q'))
             {
-                screen.ExitLoopClosure()();
+                screen_.ExitLoopClosure()();
                 return true;
             } 
 
             if (event == ftxui::Event::Character('r'))  
             {
-                // TODO: referesh
+                RefreshMonitorsInfo();
+                return true;
             }
 
             return false;
         });
         // clang-format on
 
-        screen.Loop(component);
+        screen_.Loop(component);
         StopRefreshThread();
 
         return 0;
     }
 
-    void MainScreen::StartRefreshThread(ftxui::ScreenInteractive& screen)
+    void MainScreen::RefreshMonitorsInfo()
+    {
+        using RefreshStatus = monitor::MonitorController::RefreshStatusT;
+
+        RefreshStatus status = controller_.Refresh();
+        if (status.HasFlag(RefreshStatus::kNothingChanged))
+        {
+            return;
+        }
+
+        if (status.HasFlag(RefreshStatus::kMonitorsCountOrNameChanged))
+        {
+            screen_.PostEvent(custom_events::kMonitorsCountOrNameChanged);
+        }
+        if (status.HasFlag(RefreshStatus::kMonitorInfoChanged))
+        {
+            screen_.PostEvent(custom_events::kMonitorInfoChanged);
+        }
+    }
+
+    void MainScreen::StartRefreshThread()
     {
         refresh_running_.store(true);
 
         // clang-format off
-        refresh_thread_ = std::thread([this, &screen]{ // TODO: ...
-            using RefreshStatus = monitor::MonitorController::RefreshStatusT;
-    
+        refresh_thread_ = std::thread([this] { 
             while(refresh_running_.load()) 
             {
-                std::this_thread::sleep_for(std::chrono::seconds(kRefreshInterval));                
-                RefreshStatus status = controller_.Refresh();
-
-                if (status.HasFlag(RefreshStatus::kNothingChanged)) 
-                {
-                    continue;
-                } 
-                
-                if (status.HasFlag(RefreshStatus::kMonitorsCountOrNameChanged)) 
-                {
-                    screen.PostEvent(custom_events::kMonitorsCountOrNameChanged);
-                }
-                if (status.HasFlag(RefreshStatus::kMonitorInfoChanged)) 
-                {
-                    screen.PostEvent(custom_events::kMonitorInfoChanged);
-                }
+                std::this_thread::sleep_for(std::chrono::seconds(kRefreshInterval));
+                RefreshMonitorsInfo();
             }
         });
         // clang-format on
